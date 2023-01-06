abc47eb3895d3feecf252612133d6c6432d61aea
zer0 <internetghost0@protonmail.com>
2023-01-06 06:38:52 +0000

    throw an error if file has no bridges

--- bridges.py
+++ bridges.py
@@ -11,16 +11,28 @@
 
 def get_bridges_from_file(filename='./bridges.txt'):
     bridges = []
+
+    # read bridges from file
     with open(filename, 'r') as f:
         bridges = f.readlines()
+
     # remove all leading and trailing whitespace
     bridges = map(lambda line: line.strip(), bridges)
+
     #filter all lines that does not start with 'obfs4'
     bridges = filter(lambda line: line.startswith('obfs4'), bridges)
+
+    # throw an error if variable has no bridges
+    bridges = list(bridges)
+    if len(bridges) == 0:
+        raise Exception(f"Error: `{filename}` contains 0 bridges that starts with `obfs4`")
+
     # add 'Bridge' word to all bridges
     bridges = map(lambda line: f'Bridge {line}', bridges)
+
     # list -> string
     bridges = '\n'.join(bridges)
+
     # building proper config for torrc
     bridges = f'''# get bridges from => https://bridges.torproject.org/bridges/?transport=obfs4
 
@@ -32,15 +44,20 @@ UseBridges 1
 
 {bridges}
 '''
+    # write the result to a file
     # with open('bridges_clean.txt', 'w') as f:
     #     f.write(bridges)
+
+    # print the result
     print(bridges)
 
 if __name__ == '__main__':
     try:
         get_bridges_from_file()
-    except:
+    except Exception as err:
         print('Script for easy copypasting raw bridges from https://bridges.torproject.org/bridges/?transport=obfs4')
         print('and make it proper conf for torghost or torrc')
         print()
         print('please create a `bridges.txt` file and put bridges from torproject there')
+        print('-------------------------------------------------------------------------')
+        print(err)
